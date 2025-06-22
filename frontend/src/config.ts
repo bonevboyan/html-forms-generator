8b965801a2f62ee3401cb65bfb3ecf82dbe1f5ea
Boyan Bonev <75538126+bonevboyan@users.noreply.github.com>
2025-06-23 00:56:38 +0200

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'https://html-forms-generator.onrender.com';
 export { API_URL }; 
